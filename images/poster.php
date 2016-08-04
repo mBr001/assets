821865ecf1af4259e174bf7f2b531634550f7594
Arvind Kumar <arvind@enkrypt.in>
2016-08-04 09:34:04 +0530

    Added a set of poster images.

--- images/poster.php
+++ images/poster.php
@@ -2,7 +2,7 @@
     $files = glob("posters/*.*");
     $file = array_rand($files);
     
-	//This does not handle if there is a file inside `posters/` that's not an image, and might break that is the case
+    //This does not handle if there is a file inside `posters/` that's not an image, and might break that is the case
     $imageInfo = getimagesize($files[$file]);
     header("Content-Type: ".$imageInfo['mime']);
     header("Content-Length: ".filesize($files[$file]));
